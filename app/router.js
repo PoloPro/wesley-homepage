9eb62d046649d7143ae3aee7439d4c886cadf6b6
PoloPro <wesleybdavis@gmail.com>
2016-07-02 23:43:50 -0400

    edited page structure and added resume iframe embed

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('projects');
+  this.route('skills');
+  this.route('resume');
 });
 
 export default Router;
